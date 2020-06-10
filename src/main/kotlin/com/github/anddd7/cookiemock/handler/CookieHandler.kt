119cc418919918ed9b559698ae500c1bdaa5a91e
Anddd7 <liaoad_space@sina.com>
2020-06-10 15:50:48 +0800

    add test data in cookie handler

--- src/main/kotlin/com/github/anddd7/cookiemock/handler/CookieHandler.kt
+++ src/main/kotlin/com/github/anddd7/cookiemock/handler/CookieHandler.kt
@@ -3,8 +3,6 @@ package com.github.anddd7.cookiemock.handler
 import com.github.anddd7.cookiemock.domain.Cookie
 import com.github.anddd7.cookiemock.domain.CookieBox
 import com.github.anddd7.cookiemock.domain.CookieBoxRepository
-import com.github.anddd7.cookiemock.domain.EmptyUUID
-import kotlinx.coroutines.reactive.awaitFirst
 import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.server.RequestPredicate
@@ -12,7 +10,6 @@ import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.bodyAndAwait
-import org.springframework.web.reactive.function.server.bodyToMono
 import org.springframework.web.reactive.function.server.bodyValueAndAwait
 import org.springframework.web.reactive.function.server.buildAndAwait
 import java.util.UUID
@@ -31,9 +28,39 @@ class CookieHandler(private val cookieBoxRepository: CookieBoxRepository) {
   }
 
   suspend fun save(request: ServerRequest): ServerResponse {
-    val command = request.bodyToMono<SaveCookieBoxCommand>().awaitFirst()
-    val cookieBoxes = cookieBoxRepository.save(command.toCookieBox())
-    return ok().bodyValueAndAwait(cookieBoxes)
+//    val command = request.bodyToMono<SaveCookieBoxCommand>().awaitFirst()
+//    val cookieBoxes = cookieBoxRepository.save(command.toCookieBox())
+//    return ok().bodyValueAndAwait(cookieBoxes)
+    cookieBoxRepository.save(
+      CookieBox(
+        method = HttpMethod.GET,
+        urlPattern = "/number/{x}",
+        cookies = listOf(
+          Cookie(
+            name = "x < 10",
+            condition = RequestPredicate {
+              it.pathVariable("x").toLong() < 10
+            },
+            body = "your number is less than 10"
+          ),
+          Cookie(
+            name = "x < 100",
+            condition = RequestPredicate {
+              it.pathVariable("x").toLong() < 100
+            },
+            body = "your number is less than 100"
+          ),
+          Cookie(
+            name = "x < 1000",
+            condition = RequestPredicate {
+              it.pathVariable("x").toLong() < 1000
+            },
+            body = "your number is less than 1000"
+          )
+        )
+      )
+    )
+    return ok().bodyValueAndAwait("test")
   }
 
   suspend fun delete(request: ServerRequest): ServerResponse {
@@ -44,22 +71,9 @@ class CookieHandler(private val cookieBoxRepository: CookieBoxRepository) {
 }
 
 data class SaveCookieBoxCommand(
-  val uuid: UUID = EmptyUUID
+  val uuid: UUID?
 ) {
   fun toCookieBox(): CookieBox {
-    // TODO mock data
-    return CookieBox(
-      cookies = listOf(
-        Cookie(
-          name = "case 1",
-          condition = RequestPredicate {
-            it.pathVariable("id").toLong() > 0
-          },
-          body = "this is test response"
-        )
-      ),
-      method = HttpMethod.GET,
-      urlPattern = "/test/{id}"
-    )
+    TODO()
   }
 }
